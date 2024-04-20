873172ec0d3145517aa056d49c6785d27d6eba3d
BigBossMSF <165384392+BigBossMSF@users.noreply.github.com>
2024-04-20 23:53:16 +0500

    Update views.py

--- notion 2.0/notion/main/views.py
+++ notion 2.0/notion/main/views.py
@@ -8,3 +8,17 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def note_list(request):
+    notes = Notion.objects.all()
+    return render(request, 'main/layout.html', {'notes': notes})
+
+def add_note(request):
+    if request.method == "POST":
+        form = NotionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NotionForm()
+    return render(request, 'main/add_note.html', {'form': form})

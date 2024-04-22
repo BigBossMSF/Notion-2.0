8ac6999b308a652fa481c2c1e48da77236391532
BigBossMSF <165384392+BigBossMSF@users.noreply.github.com>
2024-04-22 23:17:46 +0500

    Update urls.py

--- notion 2.0/notion/main/urls.py
+++ notion 2.0/notion/main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.note_list, name="home"),
     path('about/', views.about, name="about"),
-    path('add/', views.add_note, name='add_note')
+    path('add/', views.add_note, name='add_note'),
+    path('notes/<int:note_id>/', views.note_detail, name='note_detail')
 ]

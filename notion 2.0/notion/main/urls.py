15d932d35baa9465768f0356642844426466adad
BigBossMSF <165384392+BigBossMSF@users.noreply.github.com>
2024-04-23 17:29:26 +0500

    Update urls.py

--- notion 2.0/notion/main/urls.py
+++ notion 2.0/notion/main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.note_list, name="home"),
     path('about/', views.about, name="about"),
     path('add/', views.add_note, name='add_note'),
-    path('notes/<int:note_id>/', views.note_detail, name='note_detail')
+    path('notes/<int:note_id>/', views.note_detail, name='note_detail'),
+    path('note/delete/<int:note_id>/', views.delete_note, name='delete_note')
 ]
